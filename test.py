f49c18b566961babe5a3e302be5de001a6db3dba
Danyalss <danyalsoltani87@gmail.com>
2024-01-17 17:45:58 +0330

    add

--- test.py
+++ test.py
@@ -2,13 +2,10 @@ import ctypes
 import os
 
 SPI_SETDESKWALLPAPER = 20
-
-# image_path = os.path.join(os.getcwd(), "back")
-
-image_path = "/back.png"
+image_path = os.path.join(os.getcwd(), "back.png")
 
 # برای پایتون 2.5 و بالاتر
-ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, image_path, 0)
+# ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, image_path, 0)
 
 # برای پایتون 3.5 و بالاتر
-#ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, image_path, 0)
+ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, image_path, 0)
