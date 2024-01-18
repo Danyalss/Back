6fe5143d3b7715aa531686632ea9b6e5d62700ec
Danyalss <danyalsoltani87@gmail.com>
2024-01-18 19:58:54 +0330

    add

---
+++ to_b_up.py
@@ -0,0 +1,17 @@
+import base64
+import pyperclip
+import os
+
+os.system("dir")
+
+file_name = input("File Name : ")
+
+# باز کردن تصویر و تبدیل آن به داده باینری
+with open(file_name, "rb") as image_file:
+    binary_data = base64.b64encode(image_file.read()).decode()
+
+
+print(binary_data)
+
+# کپی کردن داده باینری به کلیپ بورد
+pyperclip.copy(binary_data)
